4ef91a00c8d42f81984a128ec2736ae53c8d92e9
Abel Masila <abelmasila@gmail.com>
2020-06-10 16:08:32 +0300

    varaints pro features

--- src/components/Order.js
+++ src/components/Order.js
@@ -11,11 +11,22 @@ const containerVariants = {
     x: 0,
     transition: {
       type: "spring",
-      delay: 0.5,
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4,
     },
   },
 };
 
+const childVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 const Order = ({ pizza }) => {
   return (
     <motion.div
@@ -25,10 +36,14 @@ const Order = ({ pizza }) => {
       animate="visible"
     >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map((topping) => (
-        <div key={topping}>{topping}</div>
-      ))}
+      <motion.p variants={childVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
     </motion.div>
   );
 };
